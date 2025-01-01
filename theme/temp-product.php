eaa515b3a75acfd8d4b61cfb0a36f597ea8cc387
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-01-01 20:02:24 +0500

    update

---
+++ theme/temp-product.php
@@ -0,0 +1,82 @@
+<?php
+/**
+* Template Name: Product
+*
+* @package WordPress
+* @subpackage Twenty_Fourteen
+* @since Twenty Fourteen 1.0
+*/
+
+get_header(); ?>
+
+<main class="max-w-[1280px] flex flex-col lg:flex-row mx-auto px-3 gap-10 mt-[140px] mb-12">
+    <div class="lg:w-[35%]">
+        <div id="page">
+            <div class="row">
+                <div class="column small-11 small-centered">
+                    <div class="slider slider-single">
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                        <div><img class="max-w-[415px] w-full object-contain mx-auto h-[415px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt="" class="h-full object-cover w-full"></div>
+                    </div>
+                    <div class="slider slider-nav mt-10 px-8">
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                        <div class="mx-[5px] !flex justify-center bg-gray-200 p-2 border border-[#B6E22E] rounded-xl overflow-hidden"><img class="rounded-xl object-cover h-[75px]" src="<?php echo get_template_directory_uri(); ?>/public/images/demo-watch.png" alt=""></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="lg:w-[65%]">
+        <div class="flex flex-col mb-2 md:flex-row justify-between gap-2 border-b pb-3 md:pb-1">
+            <div>
+                <h2 class="text-2xl font-bold">Apple Watch Ultra 2</h2>
+                <div class="flex justify-between gap-4 items-center max-w-[250px] my-2">
+                    <img src="<?php echo get_template_directory_uri(); ?>/public/svg/star-bar.svg" alt=""/>
+                    <p>246 Reviews</p>
+                </div>
+            </div>
+            <div class="flex items-center gap-4">
+                <h4 class="text-[28px] sm:text-[36px] font-bold">$ 99.99</h4>
+                <button class="border-l-[2px] px-4 border-r-[2px] border-gray-300">
+                    <a class="bg-white w-[48px] h-[48px] rounded-full flex flex-col justify-center items-center shadow">
+                        <img src="<?php echo get_template_directory_uri(); ?>/public/svg/heart2.svg" alt="" class="w-[26px] h-[16px]">
+                    </a>
+                </button>
+                <a href="#">                
+                    <span class="bg-[#B6E22E] text-black uppercase text-2xl font-light px-3 py-1.5 rounded-[8px]">Follow</span>
+                </a>
+            </div>
+        </div>
+        <div class="">
+            <h4 class="text-xl font-medium">Registry Number</h4>
+        </div>
+    </div>
+</main>
+
+
+
+
+
+
+
+
+
+<?php get_footer();
