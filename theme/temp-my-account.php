44c5e59eaedc52232c1591495d67924191ab7d54
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-01-02 15:29:46 +0500

    update

---
+++ theme/temp-my-account.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Template Name: My Account
+*
+* @package WordPress
+* @subpackage Twenty_Fourteen
+* @since Twenty Fourteen 1.0
+*/
+
+get_header(); ?>
+
+
+<?php get_template_part('template-parts/dashboard/dashboard', 'layout' ); ?>
+
+
+
+<?php get_footer();
