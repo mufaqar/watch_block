454934e6472b8b73e6059a54df3c79a0376031e8
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-01-16 19:38:26 +0500

    changes update

--- theme/page.php
+++ theme/page.php
@@ -17,67 +17,12 @@ get_header();
 
 
 <?php if (is_shop()): ?>
-    <main class="h-96 w-full !bg-top bg-no-repeat bg-cover px-3 sm:px-4 lg:px-8 2xl:px-[64px] flex flex-col justify-center text-white py-20" style="background-image: url('<?php echo get_template_directory_uri(); ?>/public/images/main.jpg')">
-    <div class="max-w-[450px] pt-20">
-        <h1 class="md:text-[64px] font-semibold text-5xl mfont md:leading-[70px]"> </h1>
-    </div>
+    <main class="h-[700px] w-full bg-no-repeat bg-cover !bg-center px-3 sm:px-4 lg:px-8 2xl:px-[64px] flex flex-col justify-center text-white py-20" style="background-image: url('<?php echo get_template_directory_uri(); ?>/public/images/main.jpg')">
+        <div class="max-w-[450px] pt-20">
+            <h1 class="md:text-[64px] font-semibold text-5xl mfont md:leading-[70px]"> </h1>
+        </div>
     </main>
-
-    <section class="bg-white">
-    <div class="max-w-[1320px] mx-auto px-3 py-10 gap-[33px] flex justify-between md:flex-nowrap flex-wrap">
-        <!-- Brands  -->
-        <div class="flex flex-col justify-between">
-            <h6 class="font-semibold mb-[9px]">Brand</h6>
-            <div class="form-select">    
-                <select class="bg-[#F9F9F9] text-[#C9C4C4] min-w-[185px] w-full py-[10px] px-[15px] font-medium rounded-[10px]">
-                    <option value="" disabled>Select brand</option>
-                    <option value="">Brand Options</option>
-                    <option value="">Brand Options</option>
-                </select>
-            </div>
-        </div>
-        <div class="w-[1.33px] bg-[#F4F4F4] h-[77px]"></div>
-        <!-- Color  -->
-        <div class=" flex flex-col justify-between">
-            <h6 class="font-semibold mb-[9px]">Color</h6>
-            <div class="flex gap-3 flex-wrap min-w-[140px]">    
-                <button class="h-[25px] w-[25px] rounded-full bg-[#FB5252]"></button>
-                <button class="h-[25px] w-[25px] rounded-full bg-[#FCA120]"></button>
-                <button class="h-[25px] w-[25px] rounded-full bg-[#FFC0CB]"></button>
-                <button class="h-[25px] w-[25px] rounded-full bg-[#FCDB7E]"></button>
-            </div>
-        </div>
-        <div class="w-[1.33px] bg-[#F4F4F4] h-[77px]"></div>
-        <!-- Condition  -->
-        <div class=" flex flex-col justify-between">
-            <h6 class="font-semibold mb-[9px]">Condition</h6>
-            <div class="flex gap-[5px] min-w-[120px]">    
-                <button class="condition-button text-[12.25px] py-[7px] px-[15px] border-[1.3px] border-[#BAC8D3] hover:bg-[#B6E22E] text-black hover:border-[#B6E22E] rounded-full">New</button>
-                <button class="condition-button text-[12.25px] py-[7px] px-[15px] border-[1.3px] border-[#BAC8D3] hover:bg-[#B6E22E] text-black hover:border-[#B6E22E] rounded-full">Used</button>
-            </div>
-        </div>
-        <div class="w-[1.33px] bg-[#F4F4F4] h-[77px]"></div>
-        <!-- Price  -->
-        <div class=" flex flex-col justify-between">
-            <h6 class="font-semibold mb-[9px]">Price</h6>
-            <div class="flex gap-[5px] min-w-[201px]">    
-                <button class="condition-button_for_price text-[12.25px] py-[7px] px-[15px] border-[1.3px] border-[#BAC8D3] hover:bg-[#B6E22E] text-black hover:border-[#B6E22E] rounded-full text-nowrap">High to Low</button>
-                <button class="condition-button_for_price text-[12.25px] py-[7px] px-[15px] border-[1.3px] border-[#BAC8D3] hover:bg-[#B6E22E] text-black hover:border-[#B6E22E] rounded-full text-nowrap">Low to High</button>
-            </div>
-        </div>
-        <div class="w-[1.33px] bg-[#F4F4F4] h-[77px]"></div>
-         <!-- Material  -->
-         <div class="flex flex-col justify-between">
-            <h6 class="font-semibold mb-[9px]">Material</h6>
-            <div class="flex gap-[5px] flex-wrap">    
-                <button class="condition-button_for_price text-[12.25px] py-[7px] px-[15px] border-[1.3px] border-[#BAC8D3] hover:bg-[#B6E22E] text-black hover:border-[#B6E22E] rounded-full">Leather</button>
-                <button class="condition-button_for_price text-[12.25px] py-[7px] px-[15px] border-[1.3px] border-[#BAC8D3] hover:bg-[#B6E22E] text-black hover:border-[#B6E22E] rounded-full">Titanium</button>
-                <button class="condition-button_for_price text-[12.25px] py-[7px] px-[15px] border-[1.3px] border-[#BAC8D3] hover:bg-[#B6E22E] text-black hover:border-[#B6E22E] rounded-full">Stainless Steel</button>
-            </div>
-        </div>
-    </div>
-</section>
-
+	<?php get_template_part( 'template-parts/product/product', 'filter' ); ?>
 <?php else: ?>
     <section class="page_title">
         <h2 class="uppercase text-center text-4xl mb-5 sm:text-5xl md:text-[64px] font-semibold text-[#2B2B2B] mt-[125px]">
---
+++ theme/template-parts/product/product-filter.php
@@ -0,0 +1,54 @@
+<section class="bg-white">
+    <div class="max-w-[1320px] mx-auto px-3 py-10 gap-[33px] flex justify-between md:flex-nowrap flex-wrap">
+        <!-- Brands  -->
+        <div class="flex flex-col justify-between">
+            <h6 class="font-semibold mb-[9px]">Brand</h6>
+            <div class="form-select">    
+                <select class="bg-[#F9F9F9] text-[#C9C4C4] min-w-[185px] w-full py-[10px] px-[15px] font-medium rounded-[10px]">
+                    <option value="" disabled>Select brand</option>
+                    <option value="">Brand Options</option>
+                    <option value="">Brand Options</option>
+                </select>
+            </div>
+        </div>
+        <div class="w-[1.33px] bg-[#F4F4F4] h-[77px]"></div>
+        <!-- Color  -->
+        <div class=" flex flex-col justify-between">
+            <h6 class="font-semibold mb-[9px]">Color</h6>
+            <div class="flex gap-3 flex-wrap min-w-[140px]">    
+                <button class="h-[25px] w-[25px] rounded-full bg-[#FB5252]"></button>
+                <button class="h-[25px] w-[25px] rounded-full bg-[#FCA120]"></button>
+                <button class="h-[25px] w-[25px] rounded-full bg-[#FFC0CB]"></button>
+                <button class="h-[25px] w-[25px] rounded-full bg-[#FCDB7E]"></button>
+            </div>
+        </div>
+        <div class="w-[1.33px] bg-[#F4F4F4] h-[77px]"></div>
+        <!-- Condition  -->
+        <div class=" flex flex-col justify-between">
+            <h6 class="font-semibold mb-[9px]">Condition</h6>
+            <div class="flex gap-[5px] min-w-[120px]">    
+                <button class="condition-button filter_button">New</button>
+                <button class="condition-button filter_button">Used</button>
+            </div>
+        </div>
+        <div class="w-[1.33px] bg-[#F4F4F4] h-[77px]"></div>
+        <!-- Price  -->
+        <div class=" flex flex-col justify-between">
+            <h6 class="font-semibold mb-[9px]">Price</h6>
+            <div class="flex gap-[5px] min-w-[201px]">    
+                <button class="condition-button_for_price filter_button text-nowrap">High to Low</button>
+                <button class="condition-button_for_price filter_button text-nowrap">Low to High</button>
+            </div>
+        </div>
+        <div class="w-[1.33px] bg-[#F4F4F4] h-[77px]"></div>
+         <!-- Material  -->
+         <div class="flex flex-col justify-between">
+            <h6 class="font-semibold mb-[9px]">Material</h6>
+            <div class="flex gap-[5px] flex-wrap">    
+                <button class="condition-button_for_price filter_button">Leather</button>
+                <button class="condition-button_for_price filter_button">Titanium</button>
+                <button class="condition-button_for_price filter_button">Stainless Steel</button>
+            </div>
+        </div>
+    </div>
+</section>
