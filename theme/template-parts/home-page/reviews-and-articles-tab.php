4dc50cdb6cbfce870d14de49e4f9b057c6814a4c
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-01-20 12:58:20 +0500

    press

--- theme/template-parts/home-page/reviews-and-articles-tab.php
+++ theme/template-parts/home-page/reviews-and-articles-tab.php
@@ -13,6 +13,21 @@ $review_args = array(
     'order'          => 'DESC', // Latest first
 );
 
+$pressargs = array(
+  'post_type'      => 'post',
+  'posts_per_page' => 3,
+  'orderby'        => 'date', // Order by date
+  'order'          => 'DESC', // Latest first
+  'tax_query'      => array(
+        array(
+            'taxonomy' => 'category', // Replace with your actual taxonomy name if different
+            'field'    => 'slug',
+            'terms'    => 'press', // The slug of the "press" category
+        ),
+    ),
+);
+
+$press = new WP_Query($pressargs);
 $review = new WP_Query($review_args);
 $query = new WP_Query($args);
 
@@ -82,9 +97,9 @@ $query = new WP_Query($args);
     <div class="ra_tab-content mt-10 mb-12 hidden" id="PRESS">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[26px]">
         <?php
-              if ($query->have_posts()) {
-                  while ($query->have_posts()) {
-                      $query->the_post();
+              if ($press->have_posts()) {
+                  while ($press->have_posts()) {
+                      $press->the_post();
                   ?>
                       <?php get_template_part( 'template-parts/articles/article', 'card' ); ?>
                   <?php }
