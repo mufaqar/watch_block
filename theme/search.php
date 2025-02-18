0c33e78ecc0afeb7262c03f1f3c67e99237bf34d
mufaqar <mufaqar@gmail.com>
2025-02-18 13:00:46 +0500

    udpated

--- theme/search.php
+++ theme/search.php
@@ -10,27 +10,32 @@
 get_header();
 ?>
 
-	<section id="primary">
-		<main id="main">
+<section id="primary">
+    <main id="main">
 
-		<?php if ( have_posts() ) : ?>
+        <?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
+      
+
+        <section class="bg-[#F2F2F2] py-14 pb-28">
+            <div class="max-w-[1280px] px-3 mx-auto">
+                <?php
 				printf(
 					/* translators: 1: search result title. 2: search term. */
-					'<h1 class="page-title">%1$s <span>%2$s</span></h1>',
+					'<h1 class="uppercase font-semibold text-[#2B2B2B] text-center text-3xl sm:text-5xl md:text-[64px]">%1$s <span>%2$s</span></h1>',
 					esc_html__( 'Search results for:', 'watch_block' ),
 					get_search_query()
 				);
 				?>
-			</header><!-- .page-header -->
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-14 gap-[52px]">
 
-			<?php
+                    <?php
 			// Start the Loop.
 			while ( have_posts() ) :
 				the_post();
-				get_template_part( 'template-parts/content/content', 'excerpt' );
+				//get_template_part( 'template-parts/content/content', 'search' );
+
+			 get_template_part('template-parts/product/product', 'card' ); 
 
 				// End the loop.
 			endwhile;
@@ -45,8 +50,11 @@ get_header();
 
 		endif;
 		?>
-		</main><!-- #main -->
-	</section><!-- #primary -->
+                </div>
+            </div>
+        </section>
+    </main><!-- #main -->
+</section><!-- #primary -->
 
 <?php
 get_footer();
--- theme/template-parts/content/content-product.php
+++ theme/template-parts/content/content-product.php
@@ -10,11 +10,5 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
-
-        <?php
-		the_content();
-		
-		?>
-    </div><!-- .entry-content -->
-
+        <?php	the_content();	?>
 </article><!-- #post-${ID} -->
