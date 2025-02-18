1c820f5109be88961ee3b4462d14ee9283d145b4
mufaqar <mufaqar@gmail.com>
2025-02-18 20:44:07 +0500

    updated

--- theme/template-parts/home-page/timepiece-spotlight.php
+++ theme/template-parts/home-page/timepiece-spotlight.php
@@ -1,29 +1,30 @@
 <section class="bg-[#1C1C1C] py-12">
     <div class="max-w-[1280px] flex flex-col lg:flex-row gap-10 px-3 mx-auto">
         <div class="lg:w-[40%]">
-            <h2 class="text-white font-semibold text-[40px] md:text-[50px] leading-[55px]">TIMEPIECE SPOTLIGHT</h2>
-            <p class="text-[26px] font-light text-white/70 my-5 leading-[28px]">One of our luxury timepieces that deserves the spotlight.</p>
-            <img src="<?php echo get_template_directory_uri(); ?>/public/svg/five-star.svg" alt="" class="w-[106px] mb-8"/>
-            <h3 class="text-white font-bold text-[28px] md:text-[32px] leading-[35px] mb-9">G M T- M A S T E R I I</h3>
-            <p class="text-lg font-light text-white/70">During the onslaught of the Japanese-led Quartz
-                Crisis of the 70s and 80’s, Swiss watchmakers for
-                forced to adapt to remain competitive in the market,
-                or let themselves be swallowed up by their competi-
-                tion. As a way to deal with the Quartz Crisis, Rolex
-                decided to release their own quartz timepiece in 1977,
-                the Oysterquartz. In 1970, Rolex had released the
-                precursor to the Oysterquartz, the Quartz Date 5100,
-                powered by the Beta 21 movement, the result of a
-                large-scale Swiss collaboration.</p>
-           <div class="flex justify-between mt-10">
-                <h4 class="text-[28px] font-bold text-white">$ 2 1 , 9 9 5</h4>
-                <a class="bg-[#B6E22E] text-[24px] mfont text-[#111111] uppercase font-semibold py-[10px] px-[9px] rounded-[12px] hover:scale-105 transition-all duration-200 ease-linear cursor-pointer">
-                    Buy now
+            <h2 class="text-white font-semibold text-[40px] md:text-[50px] leading-[55px]">SPOTLIGHT</h2>
+            <p class="text-[26px] font-light text-white/70 my-5 leading-[28px]">Discover one of our most iconic luxury timepieces that truly stands out.</p>
+            <img src="<?php echo get_template_directory_uri(); ?>/public/svg/five-star.svg" alt=""
+                class="w-[106px] mb-8" />
+            <h3 class="text-white font-bold text-[28px] md:text-[32px] leading-[35px] mb-9">GMT-MASTER II</h3>
+            <p class="text-lg font-light text-white/70">During the 1970s and 80s, Swiss watchmakers faced the fierce
+                challenge of the Japanese-led Quartz Crisis. To remain competitive and avoid being overshadowed, Rolex
+                embraced innovation. In 1977, Rolex introduced the Oysterquartz, a bold response to the shifting market
+                demands. But the story began even earlier. In 1970, they unveiled the Quartz Date 5100, powered by the
+                revolutionary Beta 21 movement, a collaborative breakthrough in Swiss watchmaking.</p>
+            <p class="text-lg font-light text-white/70">Crafted to perfection, this timepiece symbolizes resilience,
+                precision, and timeless elegance. </p>
+
+            <div class="flex justify-between mt-10">
+                <h4 class="text-[28px] font-bold text-white">$21,995</h4>
+                <a href="/shop"
+                    class="bg-[#B6E22E] text-[24px] mfont text-[#111111] uppercase font-semibold py-[10px] px-[9px] rounded-[12px] hover:scale-105 transition-all duration-200 ease-linear cursor-pointer">
+                    BUY NOW
                 </a>
-           </div>
+            </div>
         </div>
         <div class="lg:w-[60%]">
-            <img src="<?php echo get_template_directory_uri(); ?>/public/images/timepiece.png" alt="" class="h-full object-cover w-full">
+            <img src="<?php echo get_template_directory_uri(); ?>/public/images/timepiece.png" alt=""
+                class="h-full object-cover w-full">
         </div>
     </div>
 </section>
