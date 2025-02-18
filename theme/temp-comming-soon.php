88d5abf6c772e7b070a74a741351d920087225d8
mufaqar <mufaqar@gmail.com>
2025-02-18 16:26:15 +0500

    coming soon

--- theme/temp-comming-soon.php
+++ theme/temp-comming-soon.php
@@ -11,9 +11,9 @@ get_header(); ?>
 
 
 <section class="mt-60 mb-40">
-    <div class="w-full py-6 max-w-[1280px] px-3 mx-auto">
+    <div class="w-full  max-w-[1280px] px-3 mx-auto">
         <h1 class="text-4xl text-center font-bold">Comming Soon</h1>
-        <p class="text-2xl text-center font-bold">We’re almost ready to redefine your experience with luxury timepieces!
+        <p class="text-xl text-center  mt-8 leading">We’re almost ready to redefine your experience with luxury timepieces!
             WatchBlock is bringing you a secure, seamless, and stylish way to buy and sell watches. Stay connected
             something extraordinary is on its way!</p>
 
