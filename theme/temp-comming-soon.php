b37dea2d18e5138c9d70c38a4e4e236051b5d1ea
mufaqar <mufaqar@gmail.com>
2025-02-18 16:23:00 +0500

    updated

--- theme/temp-comming-soon.php
+++ theme/temp-comming-soon.php
@@ -11,8 +11,13 @@ get_header(); ?>
 
 
 <section class="mt-60 mb-40">
-    <h1 class="text-4xl text-center font-bold">Comming Soon</h1>
-    <p class="text-2xl text-center font-bold">We’re almost ready to redefine your experience with luxury timepieces! WatchBlock is bringing you a secure, seamless, and stylish way to buy and sell watches. Stay connected something extraordinary is on its way!</p>
+    <div class="w-full py-6 max-w-[1280px] px-3 mx-auto">
+        <h1 class="text-4xl text-center font-bold">Comming Soon</h1>
+        <p class="text-2xl text-center font-bold">We’re almost ready to redefine your experience with luxury timepieces!
+            WatchBlock is bringing you a secure, seamless, and stylish way to buy and sell watches. Stay connected
+            something extraordinary is on its way!</p>
+
+    </div>
 </section>
 
 
