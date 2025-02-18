4f73e1357e4e27afeeef7dd5a4a99cdb666561f2
mufaqar <mufaqar@gmail.com>
2025-02-18 16:07:57 +0500

    text

--- theme/template-parts/home-page/certified-network.php
+++ theme/template-parts/home-page/certified-network.php
@@ -3,7 +3,9 @@
         <h2 class="text-white font-semibold text-[40px] md:text-[50px]">CERTIFIED NETWORK OF <span class="text-[#111111] bg-white px-2">DEALERS</span></h2>
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 md:gap-6 mt-10">
             <div class="lg:max-w-[440px]">
-                <p class="text-[26px] mb-10 text-white font-light">Our dealers go through an extensive certificatio  proces  t  meet the WatchBlock standards. We only work with the best in the industry so you can buy with confidence.</p>
+                <p class="text-[26px] mb-10 text-white font-light">
+                Every dealer in our network is carefully vetted through a strict certification process to meet WatchBlock’s elite standards. We collaborate with top industry professionals, so you can shop with absolute confidence and peace of mind.
+                </p>
                 <a class="bg-[#B6E22E] text-[24px] mfont text-[#111111] font-semibold py-[10px] px-[9px] rounded-[12px] hover:scale-105 transition-all duration-200 ease-linear cursor-pointer">
                     FIND A DEALER NEAR YOU
                 </a>
@@ -19,23 +21,23 @@
     <div class="max-w-[1280px] text-white grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 mx-auto px-3 py-10">
         <div class="flex flex-col items-center text-center">
             <img src="<?php echo get_template_directory_uri(); ?>/public/svg/sheild.svg" alt=""/>
-            <h2 class="py-4 font-semibold text-lg uppercase">100% AUTHENTIC</h2>
-            <p class="font-light text-white/80">All our watches are verifie<br/> for authenticity in person.</p>
+            <h2 class="py-4 font-semibold text-lg uppercase">100% AUTHENTIC /AUTHENTICITY GUARANTEED</h2>
+            <p class="font-light text-white/80">Every watch is personally inspected and certified 100% authentic for your peace of mind.</p>
         </div>
         <div class="flex flex-col items-center text-center">
             <img src="<?php echo get_template_directory_uri(); ?>/public/svg/sheild.svg" alt=""/>
-            <h2 class="py-4 font-semibold text-lg uppercase">FULL TRANSPARENCY</h2>
-            <p class="font-light text-white/80">We honor fair market value <br/>of all products.</p>
+            <h2 class="py-4 font-semibold text-lg uppercase">FULL TRANSPARENCY /FAIR & TRANSPARENT PRICING</h2>
+            <p class="font-light text-white/80">We honor fair pricing, delivering unmatched clarity and value with every product.</p>
         </div>
         <div class="flex flex-col items-center text-center">
             <img src="<?php echo get_template_directory_uri(); ?>/public/svg/sheild.svg" alt=""/>
-            <h2 class="py-4 font-semibold text-lg uppercase">REAL TIME INVENTORY</h2>
-            <p class="font-light text-white/80">Every watch on our website <br/>is in stock and ready for sale.</p>
+            <h2 class="py-4 font-semibold text-lg uppercase">REAL-TIME INVENTORY</h2>
+            <p class="font-light text-white/80">Every watch listed is in stock and ready to be yours instantly. </p>
         </div>
         <div class="flex flex-col items-center text-center">
             <img src="<?php echo get_template_directory_uri(); ?>/public/svg/sheild.svg" alt=""/>
             <h2 class="py-4 font-semibold text-lg uppercase">SECURE SHIPPING</h2>
-            <p class="font-light text-white/80">We only ship with the best <br/>carriers in the industry.</p>
+            <p class="font-light text-white/80">We partner with top carriers to ensure your purchase arrives safely and on time.</p>
         </div>
     </div>
 </section>
