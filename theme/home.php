6a4433191c95b18321b08f85d775d43718804fa4
mufaqar <mufaqar@gmail.com>
2025-02-18 16:21:32 +0500

    updated

--- theme/home.php
+++ theme/home.php
@@ -16,20 +16,15 @@ get_header();
 	<section class="bg-[#1C1C1C] max-w-[1280px] mx-auto px-3 mb-20 p-[33px] grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:gap-14">
 		<div>
 			<h3 class="text-[23px] font-bold text-white uppercase text-center">Free Shipping</h3>
-			<p class="text-center text-[#FDFDFD] mt-4">Free domestic ground shipping on all orders over
-			$150.</p>
+			<p class="text-center text-[#FDFDFD] mt-4">Enjoy free domestic ground shipping on all orders over $150! Shop with ease and have your favorites delivered straight to your doorstep at no extra cost.</p>
 		</div>
 		<div>
-			<h3 class="text-[23px] font-bold text-white uppercase text-center">Path to sustainability</h3>
-			<p class="text-center text-[#FDFDFD] mt-4">We utilize sustainable materials and best practices
-			#ForethePlanet.
-			$150.</p>
+			<h3 class="text-[23px] font-bold text-white uppercase text-center">PATH TO SUSTAINABILITY</h3>
+			<p class="text-center text-[#FDFDFD] mt-4">We’re committed to making a difference. By utilizing sustainable materials and following best practices, we’re taking a step toward a greener planet. Let’s make every purchase count. #ForThePlanet.</p>
 		</div>
 		<div>
-			<h3 class="text-[23px] font-bold text-white uppercase text-center">30-day Trial</h3>
-			<p class="text-center text-[#FDFDFD] mt-4">Don't love your TRUEs? We'll take any new pair
-			(excluding Second Chance) back within 30 days.
-			$150.</p>
+			<h3 class="text-[23px] font-bold text-white uppercase text-center">30-DAY TRIAL</h3>
+			<p class="text-center text-[#FDFDFD] mt-4">Not satisfied with your purchase? Return any pair (except Second Chance items) within 30 days for a hassle-free refund.</p>
 		</div>
 	</section>
 
--- theme/temp-comming-soon.php
+++ theme/temp-comming-soon.php
@@ -1,19 +1,28 @@
 <?php
 /**
-* Template Name: Comming Soon
-*
-* @package WordPress
-* @subpackage Twenty_Fourteen
-* @since Twenty Fourteen 1.0
-*/
+ * Template Name: Coming Soon
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
 
 get_header(); ?>
 
-
 <section class="mt-60 mb-40">
-    <h1 class="text-4xl text-center font-bold">Comming Soon</h1>
+    <?php
+    // Start the loop to fetch the page's title and content
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            ?>
+            <h1 class="text-4xl text-center font-bold"><?php the_title(); ?></h1>
+            <div class="page-content">
+                <?php the_content(); ?>
+            </div>
+            <?php
+        endwhile;
+    endif;
+    ?>
 </section>
 
-
-
-<?php get_footer();
+<?php get_footer(); ?>
