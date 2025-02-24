4687d5e19e9345328aeee0a18e8f7c4c1b304b0c
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-24 19:16:12 +0500

    fixes mobile

--- js/script.js
+++ js/script.js
@@ -86,3 +86,13 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
+
+
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header-wrapper");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
--- template-parts/layout/header-content.php
+++ template-parts/layout/header-content.php
@@ -10,8 +10,9 @@
 
 ?>
 
-<header class="py-4 sm:py-5 md:py-6 lg:py-[32px] sticky w-full z-50 top-0">
-    <div class="pl-3 sm:pl-4 lg:pl-8 2xl:pl-[64px] items-center justify-between flex gap-5 lg:gap-[46px]">
+<header id="<?php echo ($header_color == 'White') ? 'white' : 'black'; ?>"
+    class="md:py-4 lg:py-[24px] sticky w-full z-50 top-[-1px]">
+    <div id="header-wrapper" class="pl-3 sm:pl-4 py-2 lg:pl-8 2xl:pl-[64px] items-center justify-between flex gap-5 lg:gap-[46px]">
         <a href="<?php bloginfo('url'); ?>">
             <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $logo?>"
                 alt="<?php bloginfo('name'); ?>" class="min-w-[120px]">
@@ -78,7 +79,7 @@
 
 <!-- Mobile Header  -->
 
-<nav id="nav-menu" class="bg-black text-white hidden lg:hidden sticky w-full z-[1]">
+<nav id="nav-menu" class="bg-black text-white hidden lg:hidden fixed md:sticky mt-[-2px] md:mt-0 w-full z-[100]">
     <div>
         <?php wp_nav_menu( array( 
 			'theme_location' => 'main', 
