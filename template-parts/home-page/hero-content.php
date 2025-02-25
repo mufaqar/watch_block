bc0a19a6e442e207fc1eda982e777770a3507b57
mufaqar <mufaqar@gmail.com>
2025-02-25 19:20:45 +0500

    update

--- template-parts/home-page/hero-content.php
+++ template-parts/home-page/hero-content.php
@@ -1,4 +1,4 @@
-<main class="xl:min-h-screen w-full !bg-top bg-no-repeat bg-cover px-3 sm:px-4 lg:px-8 2xl:px-[64px] flex flex-col justify-center text-white py-20 !mt-[-140px]" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images//main.jpg')">
+<main class="xl:min-h-screen w-full  bg-no-repeat bg-cover px-3 sm:px-4 lg:px-8 2xl:px-[64px] flex flex-col justify-center text-white py-20 !mt-[-140px] !bg-bottom" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images//main.jpg')">
     <div class="max-w-[550px] pt-20">
         <h1 class="md:text-[64px] font-semibold text-5xl mfont md:leading-[70px]"> BUY WITH CONFIDENCE </h1>
         <div class="text-[white] font-light text-[26px] mfont mt-5 mb-10">
@@ -7,7 +7,7 @@
 
            
         </div>
-        <a href="/shop" class="bg-[#B6E22E] text-[38px] mfont text-black font-semibold py-2 px-4 rounded-[12px] hover:scale-105 transition-all duration-200 ease-linear cursor-pointer">
+        <a href="/shop" class="bg-[#B6E22E] text-[24px] mfont text-black font-semibold py-2 px-4 rounded-[12px] hover:scale-105 transition-all duration-200 ease-linear cursor-pointer">
             SHOP NOW
         </a>
     </div>
