450564e8345d5068e9a3fe9d66b28534e5f83683
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-26 17:50:27 +0500

    updation

--- template-parts/home-page/logo-bar-content.php
+++ template-parts/home-page/logo-bar-content.php
@@ -1,19 +1,19 @@
 <section class="px-5 md:px-16 bg-black">
 <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-3 sm:gap-5 md:gap-10">
     <figure class="flex justify-center items-center">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/Artboard3.png" alt="" class="w-24 sm:w-32"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/1.png" alt="" class="w-24 sm:w-32"/>
     </figure>
     <figure class="flex justify-center items-center">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/Artboard1.png" alt="" class="w-full"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/4.png" alt="" class="w-40"/>
     </figure>
     <figure class="flex justify-center items-center">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/Artboard2.png" alt="" class="w-40"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/3.png" alt="" class="w-40"/>
     </figure>
     <figure class="flex justify-center items-center">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/Artboard4.png" alt="" class="w-40"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/2.png" alt="" class="w-40"/>
     </figure>
     <figure class="flex justify-center items-center">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/Artboard5.png" alt="" class="w-40"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/Brands-Logos/5.png" alt="" class="w-40"/>
     </figure>
 </div>
 </section>
@@ -21,8 +21,8 @@
 <section class="bg-white py-6 sm:py-0">
     <div class="flex flex-col sm:flex-row gap-4 justify-center items-center px-5">
         <div class="flex items-center gap-4">
-        <h4 class="md:text-2xl">YOUR PERFECT WATCH IS STILL HERE – GRAB YOURS BEFORE IT'S GONE!</h4>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/svg/right-arrow.svg" alt="" class=""/>
+            <h4 class="md:text-2xl">YOUR PERFECT WATCH IS STILL HERE – GRAB YOURS BEFORE IT'S GONE!</h4>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/svg/right-arrow.svg" alt="" class=""/>
         </div>
         <img src="<?php echo get_template_directory_uri(); ?>/images//watch.png" alt="" class=""/>
     </div>
