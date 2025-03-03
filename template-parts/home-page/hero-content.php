556ba77035a867d0ff32407eddba7d4f787a2861
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-03-03 12:28:57 +0500

    update

--- template-parts/home-page/hero-content.php
+++ template-parts/home-page/hero-content.php
@@ -1,5 +1,5 @@
 <main class="xl:min-h-screen w-full bg-no-repeat bg-cover px-5 lg:px-8 2xl:px-[64px] flex flex-col justify-center text-white py-20 !mt-[-140px] bg-center md:!bg-bottom" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images//main.jpg')">
-    <div class="max-w-[550px] pt-20">
+    <div class="max-w-[550px] pt-28">
         <h1 class="md:text-[64px] font-semibold text-3xl mfont md:leading-[70px]"> BUY WITH CONFIDENCE </h1>
         <div class="text-[white] font-light md:text-[26px] mfont mt-5 mb-10">
             <p>Experience the perfect blend of luxury and innovation with WatchBlock.</p>
