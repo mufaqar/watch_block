22a7fd265d155539052b87ce524d0b0306ed5029
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-03-10 11:53:55 +0500

    update

--- template-parts/home-page/secure-registor.php
+++ template-parts/home-page/secure-registor.php
@@ -1,18 +1,20 @@
 
 
 <section class="max-w-[1280px] mx-auto grid py-10 md:py-[86px] grid-cols-1 lg:grid-cols-2 items-center gap-10 px-5">
-    <div class="relative">
+    <a href="/shop" class="relative">
         <img src="<?php echo get_template_directory_uri(); ?>/images//secure-registr-bg.png" class="w-full"/>
         <div class="absolute -right-[1px] top-1/2 -translate-y-1/2 ">
             <div class="pl-[36px] inverRounded relative text-3xl md:!text-[46px] font-semibold text-[#111111] py-6 bg-white rounded-tl-full rounded-bl-full">
                 WATCHBLOCK
             </div>
         </div>
-    </div>
+    </a>
     <div>
-        <h2 class="text-3xl md:text-[50px] text-[#111111] font-medium md:leading-[56px]">A Secure Registry for Your Watch</h2>
-        <p class="md:text-xl font-light mb-5 mt-3">Every WatchBlock timepiece is registered on the blockchain, giving you indisputable proof of ownership and unlocking exclusive perks. Protect your investment and elevate your collection with WatchBlock.</p>
-        <a  href="/shop" class="bg-[#B6E22E] md:text-[24px] mfont text-[#111111] font-semibold py-[10px] px-[9px] rounded-[12px] hover:scale-105 transition-all duration-200 ease-linear cursor-pointer">
+        <a href="/shop">
+            <h2 class="text-3xl md:text-[50px] text-[#111111] font-medium md:leading-[56px]">A Secure Registry for Your Watch</h2>
+            <p class="md:text-xl font-light mb-5 mt-3">Every WatchBlock timepiece is registered on the blockchain, giving you indisputable proof of ownership and unlocking exclusive perks. Protect your investment and elevate your collection with WatchBlock.</p>
+        </a>
+        <a href="/shop" class="bg-[#B6E22E] md:text-[24px] mfont text-[#111111] font-semibold py-[10px] px-[9px] rounded-[12px] hover:scale-105 transition-all duration-200 ease-linear cursor-pointer">
             GET WATCHBLOCK REGISTRY
         </a>
     </div>
