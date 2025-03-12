9fa68292bc620d47e92885e726e3bfb9da002a59
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-03-12 13:02:49 +0500

    update

--- template-parts/home-page/secure-registor.php
+++ template-parts/home-page/secure-registor.php
@@ -1,7 +1,7 @@
 
 
 <section class="max-w-[1280px] mx-auto grid py-10 md:py-[86px] grid-cols-1 lg:grid-cols-2 items-center gap-10 px-5">
-    <a href="/shop" class="relative">
+    <a href="/product/g-m-t-m-a-s-t-e-r-i-i" class="relative">
         <img src="<?php echo get_template_directory_uri(); ?>/images//secure-registr-bg.png" class="w-full"/>
         <div class="absolute -right-[1px] top-1/2 -translate-y-1/2 ">
             <div class="pl-[36px] inverRounded relative text-3xl md:!text-[46px] font-semibold text-[#111111] py-6 bg-white rounded-tl-full rounded-bl-full">
@@ -10,7 +10,7 @@
         </div>
     </a>
     <div>
-        <a href="/shop">
+        <a href="/product/g-m-t-m-a-s-t-e-r-i-i">
             <h2 class="text-3xl md:text-[50px] text-[#111111] font-medium md:leading-[56px]">A Secure Registry for Your Watch</h2>
             <p class="md:text-xl font-light mb-5 mt-3">Every WatchBlock timepiece is registered on the blockchain, giving you indisputable proof of ownership and unlocking exclusive perks. Protect your investment and elevate your collection with WatchBlock.</p>
         </a>
