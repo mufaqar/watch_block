7cf446294a49e8b23c7dd8f582313b9b186b5fd9
mufaqar <mufaqar@gmail.com>
2025-03-20 11:29:18 +0500

    updated

--- template-parts/home-page/hero-content.php
+++ template-parts/home-page/hero-content.php
@@ -1,9 +1,9 @@
 <main class="xl:min-h-screen w-full bg-no-repeat bg-cover px-5 lg:px-8 2xl:px-[64px] flex flex-col justify-center text-white py-20 !mt-[-140px] bg-center md:!bg-bottom" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images//main.jpg')">
-    <div class="max-w-[550px] pt-28">
-        <h1 class="md:text-[64px] font-semibold text-3xl mfont md:leading-[70px]"> BUY WITH CONFIDENCE </h1>
-        <div class="text-[white] font-light md:text-[26px] mfont mt-5 mb-10">
-            <p>Experience the perfect blend of luxury and innovation with WatchBlock.</p>
-            <p> Each timepiece is uniquely linked to its own blockchain-backed NFT, providing secure proof of ownership and exclusive benefits.</p>           
+    <div class="max-w-[600px] pt-28">
+        <h1 class="md:text-[64px] font-semibold text-3xl mfont md:leading-[70px]"> Register Your Timepiece. Secure Its Legacy. </h1>
+        <div class="text-[white] font-light md:text-[24px] mfont mt-5 mb-10">
+            <p>Ensure your watch’s authenticity and ownership with WatchBlock’s blockchain-backed registry.</p>
+            <p>  Each timepiece is securely linked to its own digital certificate, providing tamper-proof verification, theft protection, and exclusive ownership benefits.</p>           
         </div>
         <a href="/shop" class="bg-[#B6E22E] text-[24px] mfont text-black font-semibold py-2 px-4 rounded-[12px] hover:scale-105 transition-all duration-200 ease-linear cursor-pointer">
             SHOP NOW
